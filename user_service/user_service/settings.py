352fdd9713389c45bda54947abeb1455ed33cc55
Unknown <vikhotin@yandex.ru>
2017-12-23 14:57:05 +0300

    Logging added

--- user_service/user_service/settings.py
+++ user_service/user_service/settings.py
@@ -126,3 +126,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': './logs/user_service.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
